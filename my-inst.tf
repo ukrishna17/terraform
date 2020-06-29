08958301e32750d83bdbd7a48e9e6ba9b5d132c8
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-06-29 21:06:35 +0530

    Update my-inst.tf

--- my-inst.tf
+++ my-inst.tf
@@ -8,9 +8,9 @@ resource "aws_instance" "intro" {
   ami = "ami-0ac80df6eff0e70b5"
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
-  key_name = "dino-keys"
+  key_name = "Terra-Key"
   vpc_security_group_ids = ["sg-264f5d0c"]
   tags = {
-    Name = "my-dyno"
+    Name = "my-dino"
   }
 }
