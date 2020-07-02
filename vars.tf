1c587e938ce668ecb8998de1c995f8d019064472
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-02 09:44:24 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable REGION {
+  default = "us-east-1"
+}
+variable ZONE1 {
+  default = "us-east-1a"
+}
+
+variable AMIS {
+  type = map
+  default = {
+     us-east-1 = "ami-0ac80df6eff0e70b5"
+     us-east-2 = "ami-0a63f96e85105c6d3"
+     us-west-2 = "ami-003634241a8fcdec0"
+  }
+
+}
