3e6d1e131bb36250a8739e59282c959e4df3c3bd
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-04 21:16:10 +0530

    Update secgrp.tf

--- secgrp.tf
+++ secgrp.tf
@@ -4,11 +4,18 @@ resource "aws_security_group" "vpro_stack_sg" {
   vpc_id      = aws_vpc.vprofile.id
 
   ingress {
-    description = "TLS from VPC"
+    description = "SSH from my computer"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["122.169.225.241/32"]
+  }
+  ingress {
+    description = "SSH from jenkins server "
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = "sg-0b29a2c1579c9651a"
   }
 
   egress {
