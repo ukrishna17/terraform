70fc8947035eb1b566a2a68eff9ee847e7f71b1a
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-04 20:45:42 +0530

    Update my-inst.tf

--- my-inst.tf
+++ my-inst.tf
@@ -1,15 +1,15 @@
-resource "aws_key_pair" "dino-key" {
-  key_name   = "dinokey"
-  public_key = file(var.PUB_KEY)
-}
+#resource "aws_key_pair" "dino-key" {
+#  key_name   = "dinokey"
+#  public_key = file(var.PUB_KEY)
+#}
 
 resource "aws_instance" "vpro-nginx" {
   ami = var.AMIS[var.REGION]
   instance_type = "t2.micro"
   subnet_id = aws_subnet.vprofile-pub-1.id
   availability_zone = var.ZONE1
-  key_name = aws_key_pair.dino-key.key_name
-  #key_name = "Terra-Key"
+  #key_name = aws_key_pair.dino-key.key_name
+  key_name = "Terra-Key"
   vpc_security_group_ids = [aws_security_group.vpro_stack_sg.id]
   tags = {
     Name = "my-dynos"
@@ -30,21 +30,22 @@ resource "aws_volume_attachment" "attch_vol_nginx" {
   volume_id   = aws_ebs_volume.vol_4_ngin.id
   instance_id = aws_instance.vpro-nginx.id
 }
-provisioner "file"  {
-    source = "web.sh"
-    destination = "/tmp/web.sh"
-  }
-provisioner "remote-exec" {
-    inline = [
-     "chmod u+x /tmp/web.sh",
-     "sudo /tmp/web.sh"
-    ]
-  }
-connection {
-    user = "ubuntu"
-    private_key = file(var.PRIV_KEY)
-    host = self.private_ip
-  }
+
+#provisioner "file"  {
+#    source = "web.sh"
+#    destination = "/tmp/web.sh"
+#  }
+#provisioner "remote-exec" {
+#    inline = [
+#     "chmod u+x /tmp/web.sh",
+#     "sudo /tmp/web.sh"
+#    ]
+#  }
+#connection {
+#    user = "ubuntu"
+#    private_key = file(var.PRIV_KEY)
+#    host = self.private_ip
+# }
 
 output "IP" {
   value = aws_instance.vpro-nginx.private_ip
